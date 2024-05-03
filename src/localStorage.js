bb6c7df8e134f84b96aea95871f0953ff2e6b439
Brittney Epperson <beppers87@hotmail.com>
2024-05-03 16:48:25 -0500

    Added functions to save and get from local storage

---
+++ src/localStorage.js
@@ -0,0 +1,19 @@
+const localStorageKey = "projects";
+const localStorageIdKey = "projectId";
+
+function saveToLocalStorage(projects, selectedProjectId) {
+    localStorage.setItem(localStorageKey, JSON.stringify(projects));
+    localStorage.setItem(localStorageIdKey, JSON.stringify(selectedProjectId));
+}
+
+function getFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem(localStorageKey));
+    return data;
+}
+
+function getIdFromLocalStorage() {
+    const selectedProjectId = JSON.parse(localStorage.getItem(localStorageIdKey));
+    return selectedProjectId;
+}
+
+export { saveToLocalStorage, getFromLocalStorage, getIdFromLocalStorage };
